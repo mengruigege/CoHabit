a3d3c51170843c942dbfdf1a56f6db7a61b7f1cb
RSiddenki <rsiddenk@purdue.edu>
2024-11-01 00:39:51 -0400

    Update Message.java

--- src/Message.java
+++ src/Message.java
@@ -1,6 +1,6 @@
 public interface Message {
     public boolean sendMessage(User sender, User receiver); //method for sending messages
-    public String[] getMessageLogs(); //need to implement some way of storing messages between two users; Chat object?
-    public void setMessageLogs(String[] messages); //set method for list of messages
+    public ArrayList<String> getMessageLogs(); //need to implement some way of storing messages between two users; Chat object?
+    public void setMessageLogs(ArrayList<String> messages); //set method for list of messages
     public boolean deleteMessage(String message); //removes a certain message from list of messages
 }
