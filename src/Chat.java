4869944ce7911801e22bb2d2fc4a343954855819
Keya Jadhav <jadhavk@purdue.edu>
2024-11-02 01:04:28 -0400

    Update Chat.java

--- src/Chat.java
+++ src/Chat.java
@@ -11,16 +11,18 @@ public class Chat implements Message {
         this.messages = new ArrayList<>();
     }
 
-    public boolean sendMessage(User sender, User reciever, String message) {
+    public synchronized boolean sendMessage(User sender, User reciever, String message) { 
+        //to make sure only one thread can send a message at a time
         messages.add(message);
         return true;
     }
 
-    public ArrayList<String> getMessages() {
-        return messages;
+    public synchronized ArrayList<String> getMessages() {
+        //to make sure only one thread can get a message at a time
+        return messages; //return new ArrayList<>(messages); should we put in this instead? it will make a copy of the text to be returned.
     }
 
-    public boolean deleteMessage(User sender, User reciever, String message) {
+    public synchronized boolean deleteMessage(User sender, User reciever, String message) {
         messages.remove(message);
         return true;
     }
