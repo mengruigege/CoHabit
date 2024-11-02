ca2820fa16ff92d2eff90c19dda8633543aa8230
Keya Jadhav <jadhavk@purdue.edu>
2024-11-02 01:07:32 -0400

    Update FriendList.java

--- src/FriendList.java
+++ src/FriendList.java
@@ -5,27 +5,27 @@ public class FriendList implements Friend {
     private ArrayList<User> restricted = new ArrayList<>();
     private ArrayList<User> blocked = new ArrayList<>();
 
-    public boolean addFriend(User user) {
+    public synchronized boolean addFriend(User user) {
         if (user != null && !friends.contains(user) && !blocked.contains(user)) {
             friends.add(user);
             return true;
         }
         return false;
     }
-    public boolean removeFriend(User user) {
+    public synchronized boolean removeFriend(User user) {
         return (friends.remove(user));
     }
-    public boolean restrictUser(User user) {
+    public synchronized boolean restrictUser(User user) {
         if (user != null && friends.contains(user) && !restricted.contains(user)) {
             restricted.add(user);
             return true;
         }
         return false;
     }
-    public boolean allowUser(User user) {
+    public synchronized boolean allowUser(User user) {
         return restricted.remove(user);
     }
-    public boolean blockUser(User user) {
+    public synchronized boolean blockUser(User user) {
         if (user != null && !blocked.contains(user)) {
             blocked.add(user);
             friends.remove(user);
@@ -35,16 +35,16 @@ public class FriendList implements Friend {
         return false;
 
     }
-    public boolean unblockUser(User user) {
+    public synchronized boolean unblockUser(User user) {
         return blocked.remove(user);
     }
-    public ArrayList<User> getFriends() {
+    public synchronized ArrayList<User> getFriends() {
         return friends;
     }
-    public ArrayList<User> getRestricted() {
+    public synchronized ArrayList<User> getRestricted() {
         return restricted;
     }
-    public ArrayList<User> getBlocked() {
+    public synchronized ArrayList<User> getBlocked() {
         return blocked;
     }
 
