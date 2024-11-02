a520fc65b32f27533e281503f918d8f0c1704703
Keya Jadhav <jadhavk@purdue.edu>
2024-11-02 12:10:21 -0400

    Update Chat.java

--- src/Chat.java
+++ src/Chat.java
@@ -11,8 +11,7 @@ public class Chat implements Message {
         this.messages = new ArrayList<>();
     }
 
-    public synchronized boolean sendMessage(User sender, User reciever, String message) { 
-        //to make sure only one thread can send a message at a time
+    public boolean sendMessage(User sender, User reciever, String message) { 
         messages.add(message);
         return true;
     }
@@ -22,9 +21,53 @@ public class Chat implements Message {
         return messages; //return new ArrayList<>(messages); should we put in this instead? it will make a copy of the text to be returned.
     }
 
-    public synchronized boolean deleteMessage(User sender, User reciever, String message) {
+    public boolean deleteMessage(User sender, User reciever, String message) {
         messages.remove(message);
         return true;
     }
 
 }
+
+/**
+private class SendMessageTask implements Runnable {
+        private final User sender;
+        private final User receiver;
+        private final String message;
+
+        public SendMessageTask(User sender, User receiver, String message) {
+            this.sender = sender;
+            this.receiver = receiver;
+            this.message = message;
+        }
+
+        @Override
+        public void run() {
+            synchronized (Chat.this) {
+                messages.add(message);
+                System.out.println("Message sent: " + message);
+            }
+        }
+    }
+
+    private class DeleteMessageTask implements Runnable {
+        private final User sender;
+        private final User receiver;
+        private final String message;
+
+        public DeleteMessageTask(User sender, User receiver, String message) {
+            this.sender = sender;
+            this.receiver = receiver;
+            this.message = message;
+        }
+
+        @Override
+        public void run() {
+            synchronized (Chat.this) {
+                if (messages.remove(message)) {
+                    System.out.println("Message deleted: " + message);
+                }
+            }
+        }
+    }
+}
+*/
