60a418bc9a0d133026917ff1a372a54db3accc25
AidanL2106 <aidanlefort06@gmail.com>
2024-11-03 17:48:26 -0500

    Renamed DatabaseTest --> TestDatabase

--- src/TestUserSearch.java
+++ src/TestUserSearch.java
@@ -3,26 +3,35 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 
 public class TestUserSearch {
-    private UserSearch userSearch = new UserSearch();
-    private User user1 = new User("Bob", "password123", "bob@gmail.com", "1234567890", "person", "purdue");
-    private User user2 = new User("Jim", "password234", "jim@gmail.com", "2345678901", "person2", "purdue2");
+    private static UserSearch userSearch = new UserSearch();
+    private static User user1;
+    private static User user2;
 
     static {
-        User.allUsers.add(user1);
-        User.allUsers.add(user2);
+        try {
+            user1 = new User("Bob", "password123", "bob@gmail.com", "1234567890", "person", "purdue");
+            user2 = new User("Jim", "password234", "jim@gmail.com", "2345678901", "person2", "purdue2");
+        } catch (UsernameTakenException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    static {
+        Database users = new Database();
+        users.addUser(user1);
+        users.addUser(user2);
     }
     public void testsearchParamter(){
-        ArrayList<User> search = userSearch.searchParameter("name","Bob");
+        ArrayList<User> search = userSearch.searchByParameter("name","Bob");
         assertEquals("Bob",search.get(0).getName());
-        ArrayList<User> search = userSearch.searchParameter("password","password123");
+        search = userSearch.searchByParameter("password", "password123");
         assertEquals("password123",search.get(0).getPassword());
-        ArrayList<User> search = userSearch.searchParameter("email","bob@gmail.com");
+        search = userSearch.searchByParameter("email","bob@gmail.com");
         assertEquals("bob@gmail.com",search.get(0).getEmail());
-        ArrayList<User> search = userSearch.searchParameter("phone","1234567890");
+        search = userSearch.searchByParameter("phone","1234567890");
         assertEquals("1234567890",search.get(0).getPhoneNumber());
-        ArrayList<User> search = userSearch.searchParameter("desc","person");
+        search = userSearch.searchByParameter("desc","person");
         assertEquals("person",search.get(0).getDescription());
-        ArrayList<User> search = userSearch.searchParameter("uni","purdue");
+        search = userSearch.searchByParameter("uni","purdue");
         assertEquals("purdue",search.get(0).getUniversity());
     }
     public void testExactMatch() {
