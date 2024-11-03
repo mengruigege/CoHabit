ddc0aee34679c25da6c8ad74472e2f90f54a70b5
Keya Jadhav <jadhavk@purdue.edu>
2024-11-03 14:16:41 -0500

    Update UserSearch.java

--- src/UserSearch.java
+++ src/UserSearch.java
@@ -3,80 +3,120 @@ import java.util.ArrayList;
 public class UserSearch implements Searchable {
 
     private static final Object lock = new Object();
-    
-    public ArrayList<User> searchParameter (String parameter, String value) {
-        ArrayList<User> matchingUsers = new ArrayList<>();
 
-        synchronized (lock) {
-            for (User user : User.allUsers) {
-                switch (parameter.toLowerCase()) {
-                    case "name":
-                        if (user.getName().equals(value)) {
-                            matchingUsers.add(user);
-                        }
-                        break;
-                    case "email":
-                        if (user.getEmail().equalsIgnoreCase(value)) {
-                            matchingUsers.add(user);
-                        }
-                        break;
-                    case "phone":
-                        if (user.getPhoneNumber().equals(value)) {
-                            matchingUsers.add(user);
-                        }
-                        break;
-                    case "university":
-                        if (user.getDescription().equalsIgnoreCase(value)) {
-                            matchingUsers.add(user);
-                        }
-                        break;
-                    default:
-                        System.out.println("Invalid parameter type");
-                        break;
+    private class SearchParameterTask implements Runnable {
+        private final String parameter;
+        private final String value;
+        private final ArrayList<User> matchingUsers;
+
+        public SearchParameterTask(String parameter, String value, ArrayList<User> matchingUsers) {
+            this.parameter = parameter;
+            this.value = value;
+            this.matchingUsers = matchingUsers;
+        }
+
+        public void run() {
+            synchronized (lock) {
+                for (User user : User.allUsers) {
+                    switch (parameter.toLowerCase()) {
+                        case "name":
+                            if (user.getName().equals(value)) {
+                                matchingUsers.add(user);
+                            }
+                            break;
+                        case "email":
+                            if (user.getEmail().equalsIgnoreCase(value)) {
+                                matchingUsers.add(user);
+                            }
+                            break;
+                        case "phone":
+                            if (user.getPhoneNumber().equals(value)) {
+                                matchingUsers.add(user);
+                            }
+                            break;
+                        case "university":
+                            if (user.getDescription().equalsIgnoreCase(value)) {
+                                matchingUsers.add(user);
+                            }
+                            break;
+                        default:
+                            System.out.println("Invalid parameter type");
+                            break;
+                    }
                 }
             }
         }
+    }
 
+    public ArrayList<User> searchParameter(String parameter, String value) {
+        ArrayList<User> matchingUsers = new ArrayList<>();
+        Thread thread = new Thread(new SearchParameterTask(parameter, value, matchingUsers));
+        thread.start();
+        try {
+            thread.join();  
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
         return matchingUsers;
     }
 
     public ArrayList<User> searchByParameter(String parameter, String value) {
         ArrayList<User> results = new ArrayList<>();
-
-        synchronized (lock) {
-            for (User user : User.getAllUsers()) {
-                if (mainUser != user && mainUser.perfectMatch(user)) {
-                    results.add(user);
-                }
-            }
+        Thread thread = new Thread(new SearchParameterTask(parameter, value, results));
+        thread.start();
+        try {
+            thread.join(); 
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); 
         }
         return results;
     }
 
     public ArrayList<User> exactMatch(Profile mainProfile) {
         ArrayList<User> results = new ArrayList<>();
-
-        synchronized (lock) {
-            for (User user : User.getAllUsers()) {
-                if (mainProfile != user && mainProfile.perfectMatch(user)) {
-                    results.add(user);
-                }
-            }
-        }
-        return results;
-    }
-    
-    public ArrayList<User> partialMatch(Profile mainProfile) {
-        ArrayList<User> results = new ArrayList<>();
-        for (int i = 5; i > 0 ; i--) {
-            for (User user : User.getAllUsers()) {
-                if (!(mainProfile.getName().equals(user.getName()))) {
-                    if (mainProfile.partial(user) == i) {
-                        results.add(user);
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                synchronized (lock) {
+                    for (User user : User.getAllUsers()) {
+                        if (mainProfile != user && mainProfile.perfectMatch(user)) {
+                            results.add(user);
+                        }
                     }
                 }
             }
+        });
+        thread.start();
+        try {
+            thread.join();  
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); 
         }
-       return results;
+        return results;
+    }
+
+    public ArrayList<User> partialMatch(Profile mainProfile) {
+        ArrayList<User> results = new ArrayList<>();
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 5; i > 0; i--) {
+                    synchronized (lock) {
+                        for (User user : User.getAllUsers()) {
+                            if (!(mainProfile.getName().equals(user.getName()))) {
+                                if (mainProfile.partial(user) == i) {
+                                    results.add(user);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        thread.start();
+        try {
+            thread.join();  
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); 
+        }
+        return results;
     }
 }
