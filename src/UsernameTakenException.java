fc38100e3a463ae57b41b4f42f69ebe74a3756f5
Keya Jadhav <jadhavk@purdue.edu>
2024-11-03 18:29:15 -0500

    Update UsernameTakenException.java

--- src/UsernameTakenException.java
+++ src/UsernameTakenException.java
@@ -3,7 +3,4 @@ public class UsernameTakenException extends Exception {
     public UsernameTakenException(String message) {
         super(message); 
     }
-    public UsernameTakenException() {
-        super();
-    }
 }
