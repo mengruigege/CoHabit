be111be049e8051adf6a6ff84f467529fc349f32
andrewttang <tang690@purdue.edu>
2024-11-02 21:15:42 -0400

    Update TestUser.java

--- src/TestUser.java
+++ src/TestUser.java
@@ -1,38 +1,49 @@
-import static org.junit.Assert.assertEquals;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
-public class TestUser {
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+
+public class testUser {
         private User user1 = new User("Bob", "password123", "bob@gmail.com", "1234567890", "person", "purdue");;
         private User user2 = new User("Joe", "password234", "joe@gmail.com", "2234567890", "person2", "purdue2");
 
         public void testConstructor() {
             assertEquals("Bob", user1.getName());
-            assertEquals("password123", user1.getPassword());
+            assertEquals("password123", user1.getPwd());
             assertEquals("bob@gmail.com", user1.getEmail());
-            assertEquals("1234567890", user1.getPhoneNumber());
-            assertEquals("person", user1.getDescription());
+            assertEquals("1234567890", user1.getPhoneNum());
+            assertEquals("person", user1.getUserDesc());
         }
 
-        public void testGetAndSet() {
+        public void testGetandSet() {
             user1.setName("newname");
-            user1.setPassword("newpassword");
+            user1.setPwd("newpassword");
             user1.setEmail("newemail@gmail.com");
-            user1.setPhoneNumber("1111111111");
-            user1.setDescription("newdescription");
-            user1.setUniversity("newuni");
+            user1.setPhoneNum("1111111111");
+            user1.setUserDesc("newdescription");
+            user1.setUni("newuni");
 
             assertEquals("newname", user1.getName());
-            assertEquals("newpassword", user1.getPassword());
+            assertEquals("newpassword", user1.getPwd());
             assertEquals("newemail@gmail.com", user1.getEmail());
-            assertEquals("1111111111", user1.getPhoneNumber());
-            assertEquals("Unewdescription", user1.getDescription());
+            assertEquals("1111111111", user1.getPhoneNum());
+            assertEquals("Unewdescription", user1.getUserDesc());
         }
 
-        public void testSetPreference() {
+        public void testsetPreference() {
             user1.setPreferences("10 PM", true, false, true, 3, 5);
             assertEquals("10 PM, true, false, true, 3, 5", user1.getPreferences());
         }
 
-        public void testToString() {
+        public void testtoString() {
             user1.setPreferences("10 PM", true, false, true, 3, 5);
             String expected = "Bob,password123,bob@gmail.com,1234567890,person,purdue,10 PM,true,false,true,3,5";
             assertEquals(expected, user1.toString());
