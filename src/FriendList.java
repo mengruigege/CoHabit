a9549ce9349103aead5b773e41e6c4873911ac02
Keya Jadhav <jadhavk@purdue.edu>
2024-11-03 21:15:33 -0500

    Update FriendList.java

--- src/FriendList.java
+++ src/FriendList.java
@@ -1,5 +1,16 @@
 import java.util.ArrayList;
 
+/**
+ * Team Project Phase 1 - CoHabit
+ *
+ * This program works to implement a roommate search algorithm
+ *
+ * @author Aidan Lefort, Andrew Tang, Keya Jadhav, Rithvik Siddenki, Rui Meng 
+ *
+ * @version November 3rd, 2024
+ *
+ */
+
 public class FriendList implements FriendManageable, Blockable {
     private ArrayList<User> friends;
     private ArrayList<User> blocked;
