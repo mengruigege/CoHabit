f4fd6c97396ee5ef50fcabdfa5dc3d034d4b17a0
Keya Jadhav <jadhavk@purdue.edu>
2024-11-03 20:56:35 -0500

    Update InvalidInput.java

--- src/InvalidInput.java
+++ src/InvalidInput.java
@@ -1,6 +1,7 @@
 public class InvalidInput extends Exception {
+
+    Constructs a new exception indicating invalid input.
     public InvalidInput(String message) {
         super(message);
     }
-    public InvalidInput() { super(); }
 }
