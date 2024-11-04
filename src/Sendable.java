5f99c85290d1c34160cffab4109991717621497d
Keya Jadhav <jadhavk@purdue.edu>
2024-11-03 21:17:53 -0500

    Update Sendable.java

--- src/Sendable.java
+++ src/Sendable.java
@@ -1,5 +1,16 @@
 import java.util.*;
 
+/**
+ * Team Project Phase 1 - CoHabit
+ *
+ * This program works to implement a roommate search algorithm
+ *
+ * @author Aidan Lefort, Andrew Tang, Keya Jadhav, Rithvik Siddenki, Rui Meng 
+ *
+ * @version November 3rd, 2024
+ *
+ */
+
 public interface Sendable {
     boolean sendMessage(User sender, User receiver, String message); //to send messages
     boolean deleteMessage(User sender, User receiver, String message); //to delete messages
