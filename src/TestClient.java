df1df4283d7a7054c00c4ad69e75af78924ddf86
Keya Jadhav <jadhavk@purdue.edu>
2024-11-18 23:28:06 -0500

    Update TestClient.java

--- src/TestClient.java
+++ src/TestClient.java
@@ -4,6 +4,15 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+ * Team Project Phase 1 - CoHabit
+ * <p>
+ * This program works to implement a roommate search algorithm
+ *
+ * @author Aidan Lefort, Andrew Tang, Keya Jadhav, Rithvik Siddenki, Rui Meng
+ * @version November 3rd, 2024
+ */
+
 public class TestClient {
     private Client client;
 
