e36c0744ce726eca52293f6ad8675ecd0793d980
Rui Meng <34930961+mengruigege@users.noreply.github.com>
2024-11-19 23:56:09 -0500

    Update ServerService.java

--- src/ServerService.java
+++ src/ServerService.java
@@ -1,5 +1,14 @@
 import java.util.*;
 
+/**
+ * Team Project Phase 2 - CoHabit
+ * <p>
+ * This program works to implement a roommate search algorithm
+ *
+ * @author Aidan Lefort, Andrew Tang, Keya Jadhav, Rithvik Siddenki, Rui Meng
+ * @version November 3rd, 2024
+ */
+
 public interface ServerService {
     void start(int port);                                  // Start the server
 
