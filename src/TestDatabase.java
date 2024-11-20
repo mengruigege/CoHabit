41322e319773b766a057996908b6e553e33223f1
Rui Meng <34930961+mengruigege@users.noreply.github.com>
2024-11-19 23:56:32 -0500

    Update TestDatabase.java

--- src/TestDatabase.java
+++ src/TestDatabase.java
@@ -6,6 +6,15 @@ import static org.junit.Assert.*;
 import java.io.File;
 import java.util.ArrayList;
 
+/**
+ * Team Project Phase 2 - CoHabit
+ * <p>
+ * This program works to implement a roommate search algorithm
+ *
+ * @author Aidan Lefort, Andrew Tang, Keya Jadhav, Rithvik Siddenki, Rui Meng
+ * @version November 3rd, 2024
+ */
+
 public class TestDatabase {
     private Database database;
     private User user1;
