7d923da832e072bbcc392c47181b2afa1d9aba4f
Rui Meng <34930961+mengruigege@users.noreply.github.com>
2024-11-19 23:55:27 -0500

    Update ClientService.java

--- src/ClientService.java
+++ src/ClientService.java
@@ -1,5 +1,14 @@
 import java.util.*;
 
+/**
+ * Team Project Phase 2 - CoHabit
+ * <p>
+ * This program works to implement a roommate search algorithm
+ *
+ * @author Aidan Lefort, Andrew Tang, Keya Jadhav, Rithvik Siddenki, Rui Meng
+ * @version November 3rd, 2024
+ */
+
 public interface ClientService {
     boolean connect(String serverAddress, int port);          // Connect to server
 
