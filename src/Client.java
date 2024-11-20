b67657385adea7ce7834fad52e231da4e3cfbc7e
andrew <andrewtang1211@gmail.com>
2024-11-19 22:05:43 -0500

    updates

--- src/Client.java
+++ src/Client.java
@@ -610,7 +610,7 @@ public class Client implements ClientService {
             return false;
         }
 
-        out.println("login," + usernameInput + "," + passwordInput);
+        out.println("login," + username + "," + password);
 
         try {
             String response = in.readLine(); //To read response from server
